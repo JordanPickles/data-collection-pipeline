4dc225bcf43201911b57e8b16c2b4e4ed7c6fb14
JordanPickles <jordan.l.pickles@gmail.com>
2022-12-07 10:26:10 +0000

    Updates docstrings and adds typings

--- CoinMarketCapScraper.py
+++ CoinMarketCapScraper.py
@@ -19,10 +19,9 @@ class CoinMarketScraper:
     
     def __init__(self):
         """
-        This method initialisess the CoinMarketScraper Class and passing the attributes
-        Attributes passed through into the class below
-        ----------------------------------------------
-        driver: WebDriver
+        This method initialises the class and passes the following attributes.
+        ----------------------------------------------------------------------
+        driver: WebDriver.Chrome()
             This webdriver is used to load and naviagte the webpage allowing the data to be collected
         
         url: string
@@ -44,12 +43,9 @@ class CoinMarketScraper:
 
 
     def load_webpage(self) -> str: 
-        """
-        This method calls the chromedriver to load the webpage and performs a check to ensure the webpage has loaded successfully or not. 
-        If the pop up does not occure then the argument is passed
-        """
+        """Loads the webpage using chromedriver"""
         try:
-            page = self.driver.get(self.url)
+            self.driver.get(self.url)
             WebDriverWait(self.driver, 20).until(EC.presence_of_element_located((By.XPATH, '/html/body')))
             return 'The page was loaded successfully'
         except TimeoutException:
@@ -58,11 +54,12 @@ class CoinMarketScraper:
     
     def create_list_of_webpage_links(self):
         """
-        This method scrapes the webpage links for the following pages on the coinmarketcap.com site. 
-        This method then appends the links to self.page_links_list which can then be accessed at another time.
-
-        To do this, the method identifies the li tags which are present for each page of the website.
-        Within the li tags, all of the a tags are stored, the a tags contain the desired urls for each page.
+        This method collates links for following pages on coinmarketcap.com and appends them to a list.
+        ------------------------------------------------------------------------------------------------------
+        page_number_bar: HTML Tree
+            Locates the HTML tree in which the page urls are found
+        li_tags: HTML tags
+            Locates all of the li tags stored witin the page_number_bar tree. Within these tags, all of the page urls are stored.
         """  
         self.driver.execute_script("window.scrollTo(0, document.body.scrollHeight);") 
         time.sleep(3)
@@ -75,13 +72,21 @@ class CoinMarketScraper:
             self.page_links_list.append(link)
         
         
-    def create_list_of_coin_links(self) -> list:
+    def create_list_of_coin_links(self):
         """
-        This method scrapes the page provided for the cryptocurrency coin links that are stored on that page.
-        To do this, the method finds the table in the html code within which all of these links are stored in.
-        Once the table is found, it is iterated through to find the links and then appends the links to the link list.
-        The link list is a local variable list to this method, for the current page provided, all of the coin links on the page are added to this list.
-        The link_list for that page is then added to the self.coin_link_list through .extend in which this list contains all of the coin links on all of the pages iterated through.
+        This method scrapes the page provided for the cryptocurrency coin links that are stored on that page. The links on each page are added to the self.coin_link_list
+        --------------------------------------------------------------------------------------------------------
+        link_list: List
+            Local list to this method used store the coin page urls on each page before being extended to the bigger self.coin_link_list
+
+        table: HTML Tree
+            Locates the HTML tree containing the table in which urls are stored
+
+        table_rows: HTML table rows
+            Locates the table rows containing the information for each coin, including the page urls for each coin
+
+        link: String
+            Extracts the page link from the table row
         """ 
         link_list = [] 
         ignored_exceptions= (StaleElementReferenceException)
@@ -90,20 +95,15 @@ class CoinMarketScraper:
         self.driver.execute_script("window.scrollTo(0, document.body.scrollHeight);") 
         time.sleep(2)
         table = self.driver.find_element(by=By.XPATH, value='//*[@id="__next"]/div/div[1]/div[2]/div/div[1]/div[4]/table/tbody') 
-        table_list = table.find_elements(by=By.XPATH, value = './tr') 
-        for coin in table_list: 
+        table_rows = table.find_elements(by=By.XPATH, value = './tr') 
+        for coin in table_rows: 
             link = coin.find_element(by=By.TAG_NAME, value = 'a').get_attribute('href') 
             link_list.append(link) 
         self.coin_link_list.extend(link_list) 
-        return self.coin_link_list
         
 
     def webpage_links_iteration(self): 
-        """
-        This method iterates through the page_links_list created in the create_list_of_webpage_links function above.
-        The driver attribute is called to load each page.
-        Once each page has been loaded, the create_list_of_coin_links function is called
-        """
+        """This method iterates through the page_links_list created in the create_list_of_webpage_links() method and calls the create_list_of_coin_links() after each new page is loaded."""
 
         for page in self.page_links_list[:2]: #
             self.driver.get(page)
@@ -112,7 +112,7 @@ class CoinMarketScraper:
         
 
     def load_coin_webpage(self, coin_link) -> str:
-        "Loads the url provided during the coin_link_iteration() method when called"
+        """This method loads the url provided during the coin_link_iteration() method when called. It checks for an expected pop-up and closes it if the pop-up is located"""
         self.driver.get(coin_link)
         try:
             pop_up_button = self.driver.find_element(by=By.XPATH, value = '/html/body/div[3]/div/div/div/div/button[2]')
@@ -129,23 +129,26 @@ class CoinMarketScraper:
      
     def scrape_webpage_data(self) -> dict: 
         """
-        The following method scrapes the data for the desired metric for the coin page provided.
-        Using the .find_element function along with locators within selenium, the location of the desired metric is sourced.
+        This method scrapes the data for the desired metric for the coin page provided. The data is added to the self.data_dict.
+        ---------------------------------------------------------------------------------------------------------------------
+        Name: String
+            Cryptocurrency Coin Name
+        Price: String
+            Cryptocurrency Coin Price
+        Market cap: String
+            Crytpocurrency Coin Market Cap
+        24 Hour trading volume: String
+            Cryptocurrency Coin 24 Hour Trading Volume
+        24 Hour price low: String
+            Cryptocurrency Coin 24 Hour Price Low
+        24 Hour pricee high: String
+            Cryptocurrency Coin 24 Hour Price Low
+        Image url: String
+            Cyrptocurrency Coin Image URL's
+        Timestamp: Strinng
+            Timestamp of when Cryptocurrency Coin Data is Scraped
 
-        Metrics scraped for each coin:
-        Name
-        Price
-        Market cap
-        24 Hour trading volume
-        24 Hour price low
-        24 Hour pricee high
-        Image url: The urls for the images are found, the download_image_from_webpage method is then called to download the image to the local device
-        Timestamp of the data scrape
-
-        The data collected is then added to the local webpage_data_dict ditcionary.
-
-        The dictionary is returned at the end of the method.       
-        
+        Returns: self.data_dict is returned at the end of the method containing the data scraped from each Cryptocurrency Coin page.
         """
         coin_name = self.driver.find_element(by=By.XPATH, value = '//*[@class="sc-aba8b85a-0 gmYubB h1"]/span/span').text
         price = self.driver.find_element(by=By.XPATH, value = '//*[@class="priceValue "]').text                                                          
@@ -171,10 +174,10 @@ class CoinMarketScraper:
     def download_image_from_webpage(self, coin_img_src, fp):
 
         """
-        This method calls the coin image src and the filepath set in the get data method to download the image stored on each coin page.
-        Using a the os context manager a folder is created in the repository to store the images in, should there not alreadt be a folder with this name created
-        Using the driver and calling the coin_image_src located earlier, the content of the link (the image) is collected
-        The image is then stored with in the folder created and is named using the coin name and timestamp data in the scrape_webpage_data method
+        This method is called during the scrape_webpage_data() method. This method downloads the image from the the image URL and stores this locally using the os context manager
+        -----------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+        coin_img_data: .jpg file
+            Downloads the image using requests.get
         """
 
         if os.path.exists("images") == False: 
@@ -187,14 +190,8 @@ class CoinMarketScraper:
 
 
     def coin_link_iteration(self):
-        """
-        This method iterates through each coin link collected in the create_list_of_coin_links method by loading the url through the WebDriver. 
-        For each page iterated through, there is code to close the pop-up that appears.
-        The scrape_webpage_data method is then called once the page structure has fully loaded
-        The data dictionary returned by the scrape_webpage_data method is then appeneded to the self.coin_data list
-        The self.coin_data_list data is then dumped into a JSON file in a raw_data folder, both created using the OS context manager
+        """This method iterates through each coin link collected in the create_list_of_coin_links() and calls the load_coin_webpage() and scrape_webpage_data() methods. The data from each coin link is appended to the self.coin_data list and this list is placed into a .json file and stored locally."""
         
-        """
         for coin_link in self.coin_link_list: 
             self.load_coin_webpage(coin_link)
             self.coin_data.append(self.scrape_webpage_data()) 
